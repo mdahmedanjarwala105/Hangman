46c7723a8f5f0552a051ac94ecc7e3dd77b2f2b2
Mohammed Ahmed Anjarwala <mdahmedanjarwala105@gmail.com>
2025-05-07 23:21:00 +0530

    adding formation in code

--- Pygame2.py
+++ Pygame2.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import random
+
 # import nltk --> this needs to be import so that we can download all the words in nltk
 from nltk.corpus import words
 
@@ -26,11 +27,11 @@ pygame.init()
 # Create the Game Screen
 
 width, height = 800, 600
-screen = pygame.display.set_mode((width, height)) # Creates Screen
+screen = pygame.display.set_mode((width, height))  # Creates Screen
 
 # Create Title and Icon
 pygame.display.set_caption("Hangman")
-icon = pygame.image.load("man.png") # Loads image
+icon = pygame.image.load("man.png")  # Loads image
 pygame.display.set_icon(icon)
 
 # colors
@@ -60,25 +61,27 @@ images = []
 for i in range(7):
     image = pygame.image.load("hangman" + str(i) + ".png")
     images.append(image)
-    
+
 
 # Function to draw the menu screen
 def drawBig():
     screen.fill((WHITE))  # WHITE background
     text = TITLE_FONT.render("HangMan Game", 1, BLACK)
-    screen.blit(text, (width/2 - text.get_width()/2, 20))
+    screen.blit(text, (width / 2 - text.get_width() / 2, 20))
     pygame.draw.circle(screen, BLACK, (BIG_X_Axis, BIG_Y_Axis), BIG_RADIUS, 3)
     text = LETTER_FONT.render("PLAY", 1, BLACK)
-    screen.blit(text, (BIG_X_Axis - text.get_width()/2, BIG_Y_Axis - text.get_height()/2))
+    screen.blit(
+        text, (BIG_X_Axis - text.get_width() / 2, BIG_Y_Axis - text.get_height() / 2)
+    )
     pygame.display.update()
-    
+
 
 # Function to draw the game screen
 def draw():
     screen.fill((WHITE))  # WHITE background
     text = TITLE_FONT.render("HangMan Game", 1, BLACK)
-    screen.blit(text, (width/2 - text.get_width()/2, 20))
-    
+    screen.blit(text, (width / 2 - text.get_width() / 2, 20))
+
     display_word = ""
     for letter in words:
         if letter in guessed:
@@ -87,30 +90,31 @@ def draw():
             display_word += "_ "
     text = WORD_FONT.render(display_word, 1, BLACK)
     screen.blit(text, (250, 200))
-    
-    
+
     for letter in letters:
         x, y, ltr, visible = letter
         if visible:
             pygame.draw.circle(screen, BLACK, (x, y), RADIUS, 3)
             text = LETTER_FONT.render(ltr, 1, BLACK)
-            screen.blit(text, (x - text.get_width()/2, y - text.get_height()/2))
-    
+            screen.blit(text, (x - text.get_width() / 2, y - text.get_height() / 2))
+
     if hangman_status < len(images):
         screen.blit(images[hangman_status], (50, 100))
-    
+
     pygame.display.update()
-    
-    
+
+
 # Function to display a message when the game is won or lost
 def display_message(message):
     pygame.time.delay(500)
     screen.fill(WHITE)
     text = WORD_FONT.render(message, 1, BLACK)
-    screen.blit(text, (width/2 - text.get_width()/2, height/2 - text.get_height()/2))
+    screen.blit(
+        text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2)
+    )
     pygame.display.update()
     pygame.time.delay(3000)
-    
+
 
 # Main game function
 def main():
@@ -118,32 +122,35 @@ def main():
     hangman_status = 0
     guessed = []
     words = random.choice(WORDS)
-    
+
     # Buttons and It's visibility
     letters = []
     for i in range(26):
         x = startX + GAP * 2 + ((RADIUS * 2 + GAP) * (i % 13))
         y = startY + ((i // 13) * (GAP + RADIUS * 2))
         letters.append([x, y, chr(A + i), True])
-    
+
     FPS = 60
     clock = pygame.time.Clock()
     # Run the Screen and the events performed on the screen
-    running = True # When I quit the window I want this to be false so that while loop stops running
+    running = True  # When I quit the window I want this to be false so that while loop stops running
 
     while running:
-        
+
         clock.tick(FPS)
         # Fill the screen with a color to prevent ghosting
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: # closes the screen
+            if event.type == pygame.QUIT:  # closes the screen
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x_axis, mouse_y_axis = pygame.mouse.get_pos()
                 for letter in letters:
                     x, y, ltr, visible = letter
                     if visible:
-                        distance = math.sqrt((math.pow(x - mouse_x_axis,2)) + (math.pow(y - mouse_y_axis, 2)))
+                        distance = math.sqrt(
+                            (math.pow(x - mouse_x_axis, 2))
+                            + (math.pow(y - mouse_y_axis, 2))
+                        )
                         if distance < RADIUS:
                             letter[3] = False
                             guessed.append(ltr)
@@ -151,43 +158,49 @@ def main():
                                 hangman_status += 1
                                 if hangman_status == 6:
                                     draw()
-                                    pygame.time.delay(500)  # Delay to display the final image
+                                    pygame.time.delay(
+                                        500
+                                    )  # Delay to display the final image
                                     display_message("You Lost")
                                     return
-        
+
         draw()
-                                
+
         won = True
         for letter in words:
             if letter not in guessed:
                 won = False
                 break
-        
+
         if won:
             display_message("You Won")
             return
-            
+
 
 # Menu function to display the start screen
 def menu():
     FPS = 60
     clock = pygame.time.Clock()
     # Run the Screen and the events performed on the screen
-    running = True # When I quit the window I want this to be false so that while loop stops running
+    running = True  # When I quit the window I want this to be false so that while loop stops running
 
     while running:
-        
+
         clock.tick(FPS)
         # Fill the screen with a color to prevent ghosting
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: # closes the screen
+            if event.type == pygame.QUIT:  # closes the screen
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x_axis, mouse_y_axis = pygame.mouse.get_pos()
-                distance = math.sqrt((math.pow(BIG_X_Axis - mouse_x_axis,2)) + (math.pow(BIG_Y_Axis - mouse_y_axis, 2)))
+                distance = math.sqrt(
+                    (math.pow(BIG_X_Axis - mouse_x_axis, 2))
+                    + (math.pow(BIG_Y_Axis - mouse_y_axis, 2))
+                )
                 if distance < BIG_RADIUS:
                     main()
-                    
+
         drawBig()  # Draw the menu screen
-        
+
+
 menu()  # Call the menu function to start the game
